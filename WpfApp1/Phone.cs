ac47ebcf4e4a8e99e409a7f57e7d543ed4c748b7
Xantrall <bauksutat@mail.ru>
2024-01-19 20:38:02 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Phone.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace WpfApp1
+{
+    public class Phone : BindableBase
+    {
+        private string _title;
+        private string _model;
+        private int _price;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                RaisePropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Model { 
+            get 
+            { 
+                return _model;
+            }
+            set 
+            { 
+                _model = value;
+                RaisePropertyChanged(nameof(Model));
+            } 
+        }
+
+        public int Price {
+            get { return _price; }
+            set
+            { 
+                _price = value;
+                RaisePropertyChanged(nameof(Price));
+            } 
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) 
+                PropertyChanged(this, new PropertyChangedEventArgs(prop)); 
+        }
+    }
+}
---
+++ WpfApp1/PhoneViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace WpfApp1
+{
+    public class PhoneViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(
+                    obj =>
+                    {
+                        Phone phone = new Phone();
+                        Phones.Insert(0, phone);
+                        SelectedPhone = phone;
+                    }));
+            }
+        }
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                RaisePropertyChanged(nameof(selectedPhone));
+            }
+        }
+
+        public PhoneViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone{Title = "Iphone", Model = "X", Price = 100},
+                new Phone{Title = "Samsung", Model = "S23", Price = 1000},
+                new Phone{Title = "Xiaomi", Model = "12T", Price = 500}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
